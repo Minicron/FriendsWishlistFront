2cffc1f27a70929e9ade7e0fdf66732b5268c564
Alexis Montuy <montuy.alexis@gmail.com>
2023-10-27 01:24:45 +0200

    - Fix description tooltip when page is scrolled down - Fix masonry by removing old Tooltip component

--- src/components/WishlistDetail.js
+++ src/components/WishlistDetail.js
@@ -5,8 +5,6 @@ import AddItemForm from '../Form/AddItemForm';
 import { BsInfoCircle, BsLink, BsFillCartFill, BsCartX, BsPencil, BsTrashFill, BsBoxArrowUpRight  } from 'react-icons/bs';
 import Masonry from 'react-masonry-css';
 import EditItemForm from "../Form/EditItemForm";
-import { Tooltip, TooltipProvider } from 'react-tooltip';
-
 
 const WishlistDetail = ({ wishlist, onBackClick }) => {
 
@@ -20,10 +18,8 @@ const WishlistDetail = ({ wishlist, onBackClick }) => {
     const [hoveredItemId, setHoveredItemId] = useState(null);
     const [tooltipPosition, setTooltipPosition] = useState({ top: 0, left: 0 });
 
-
-
     const breakpointColumnsObj = {
-        default: 3,  // 3 colonnes par défaut
+        default: 4,  // 3 colonnes par défaut
         1100: 3,
         700: 2,
         500: 1
@@ -153,12 +149,16 @@ const WishlistDetail = ({ wishlist, onBackClick }) => {
 
     const handleMouseOver = (itemId, e) => {
         const rect = e.currentTarget.getBoundingClientRect();
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
         setTooltipPosition({
-            top: rect.top - 38, // décaler légèrement vers le haut
-            left: rect.right - 30 // décaler légèrement vers la droite
+            top: rect.top + scrollTop - 38, // décaler légèrement vers le haut
+            left: rect.right + scrollLeft - 30 // décaler légèrement vers la droite
         });
         setHoveredItemId(itemId);
     };
+
 
     const handleMouseOut = () => {
         setHoveredItemId(null);
@@ -266,106 +266,102 @@ const WishlistDetail = ({ wishlist, onBackClick }) => {
                         className="masonry-grid"
                         columnClassName="masonry-grid_column"
                     >
-                        <TooltipProvider>
-                            {affectedUsers.map((user) => (
-                                <div key={user.id} className="bg-white border border-gray-300 p-2 rounded-md shadow-md min-h-fit tuile">
-                                    {loggedInUser != null && loggedInUser.id === user.id ? (
-                                        <h3 className="text-lg font-semibold">{user.username} (you) </h3>
-                                    ) : (
-                                        <h3 className="text-lg font-semibold">{user.username}</h3>
-                                    )}
-                                    <ul>
-                                        {items
-                                            .filter((item) => item.userId === user.id)
-                                            .map((item) => (
-                                                <li className="bg-white mt-1 border border-gray-300 p-2 rounded-md hover:bg-gray-100 hover:shadow-md flex flex-col md:flex-row items-center justify-between text-sm max-h-[300px] overflow-hidden">
-                                                    <div className="flex flex-col pr-4 flex-grow mb-2 md:mb-0 md:flex-grow">
-                                                        <div className="flex flex-row items-center justify-between">
-                                                            <p className="break-word">{item.itemName}
-                                                                {item.reserved && loggedInUser != null && loggedInUser.id !== user.id && (
-                                                                    <span className="text-xs text-green-600"> - Reserved by {item.Reservations[0].User.username}</span>
-                                                                )}
-                                                            </p>
-                                                        </div>
-                                                    </div>
-                                                    <div className="flex items-center space-x-2">
-                                                        {item.description && (
-                                                            <button
-                                                                className="ml-2 px-2 py-1 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none"
-                                                                onMouseOver={(e) => handleMouseOver(item.id, e)}
-                                                                onMouseOut={handleMouseOut}
-                                                            >
-                                                                <BsInfoCircle size="16" />
-                                                            </button>
-                                                        )}
-                                                        {hoveredItemId === item.id && (
-                                                            <div style={{
-                                                                position: 'absolute',
-                                                                top: `${tooltipPosition.top}px`,
-                                                                left: `${tooltipPosition.left}px`,
-                                                                backgroundColor: 'white',
-                                                                border: '1px solid gray',
-                                                                padding: '5px',
-                                                                borderRadius: '5px',
-                                                                boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
-                                                                zIndex: 10,
-                                                                animation: 'fadeIn 0.5s forwards'
-                                                            }}>
-                                                                {item.description}
-                                                            </div>
-                                                        )}
-                                                        {item.url && (
-                                                            <a
-                                                                href={item.url}
-                                                                target="_blank"
-                                                                rel="noopener noreferrer"
-                                                                className="ml-2 px-2 py-1 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none"
-                                                            >
-                                                                <BsLink size="16" />
-                                                            </a>
-                                                        )}
-                                                        {!item.reserved && loggedInUser != null && loggedInUser.id !== user.id && (
-                                                            <button
-                                                                className="ml-2 px-2 py-1 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none"
-                                                                onClick={() => handleReserveItem(item.id)}
-                                                            >
-                                                                <BsFillCartFill size="16" />
-                                                            </button>
-                                                        )}
-                                                        {item.reserved && loggedInUser != null && loggedInUser.id !== user.id && (
-                                                            <>
-                                                                {item.Reservations[0].User.id === loggedInUser.id ? (
-                                                                    <button
-                                                                        className="ml-2 px-2 py-1 bg-red-500 text-white rounded-md shadow-md hover:bg-red-600 focus:outline-none"
-                                                                        onClick={() => handleCancelReserveItem(item.id)}
-                                                                    >
-                                                                        <BsCartX size="16" />
-                                                                    </button>
-                                                                ) : (
-                                                                    <p className="text-xs text-gray-500 ml-2">{item.Reservations[0].User.username}</p>
+                        {affectedUsers.map((user) => (
+                            <div key={user.id} className="bg-white border border-gray-300 p-2 rounded-md shadow-md min-h-fit tuile">
+                                {loggedInUser != null && loggedInUser.id === user.id ? (
+                                    <h3 className="text-lg font-semibold">{user.username} (you) </h3>
+                                ) : (
+                                    <h3 className="text-lg font-semibold">{user.username}</h3>
+                                )}
+                                <ul>
+                                    {items
+                                        .filter((item) => item.userId === user.id)
+                                        .map((item) => (
+                                            <li className="bg-white mt-1 border border-gray-300 p-2 rounded-md hover:bg-gray-100 hover:shadow-md flex flex-col md:flex-row items-center justify-between text-sm max-h-[300px] overflow-hidden">
+                                                <div className="flex flex-col pr-4 flex-grow mb-2 md:mb-0 md:flex-grow">
+                                                    <div className="flex flex-row items-center justify-between">
+                                                        <p className="break-word">{item.itemName}
+                                                            {item.reserved && loggedInUser != null && loggedInUser.id !== user.id && (
+                                                                <span className="text-xs text-green-600"> - Reserved by {item.Reservations[0].User.username}</span>
                                                             )}
-                                                            </>
-                                                        )}
-                                                        {loggedInUser.id === item.userId && (
-                                                            <>
-                                                                <button onClick={() => handleEditItemClick(item)} className="ml-2 px-2 py-1 bg-red-500 text-white rounded-md shadow-md hover:bg-red-600 focus:outline-none">
-                                                                    <BsPencil size="16" />
-                                                                </button>
-                                                                <button onClick={() => handleDeleteItemClick(item)} className="ml-2 px-2 py-1 bg-red-500 text-white rounded-md shadow-md hover:bg-red-600 focus:outline-none">
-                                                                    <BsTrashFill size="16" />
-                                                                </button>
-                                                            </>
-                                                        )}
+                                                        </p>
                                                     </div>
-                                                </li>
-                                            ))}
-                                    </ul>
-                                    {loggedInUser != null && loggedInUser.id === user.id && (
-                                        <button className="px-2 mt-1 py-2 bg-emerald-400 text-white rounded-md shadow-md hover:bg-emerald-600 min-w-full focus:outline-none" onClick={handleAddItem}> + </button>
-                                    )}
-                                </div>
-                            ))}
-                        </TooltipProvider>
+                                                </div>
+                                                <div className="flex items-center space-x-2">
+                                                    {item.description && (
+                                                        <button
+                                                            className="ml-2 px-2 py-1 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none"
+                                                            onMouseOver={(e) => handleMouseOver(item.id, e)}
+                                                            onMouseOut={handleMouseOut}
+                                                        >
+                                                            <BsInfoCircle size="16" />
+                                                        </button>
+                                                    )}
+                                                    {hoveredItemId === item.id && (
+                                                        <div style={{
+                                                            position: 'absolute',
+                                                            top: `${tooltipPosition.top}px`,
+                                                            left: `${tooltipPosition.left}px`,
+                                                            backgroundColor: 'white',
+                                                            border: '1px solid gray',
+                                                            padding: '5px',
+                                                            borderRadius: '5px',
+                                                            boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+                                                            zIndex: 10,
+                                                            animation: 'fadeIn 0.5s forwards'
+                                                        }}>
+                                                            {item.description}
+                                                        </div>
+                                                    )}
+                                                    {item.url && (
+                                                        <a
+                                                            href={item.url}
+                                                            target="_blank"
+                                                            rel="noopener noreferrer"
+                                                            className="ml-2 px-2 py-1 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none"
+                                                        >
+                                                            <BsLink size="16" />
+                                                        </a>
+                                                    )}
+                                                    {!item.reserved && loggedInUser != null && loggedInUser.id !== user.id && (
+                                                        <button
+                                                            className="ml-2 px-2 py-1 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none"
+                                                            onClick={() => handleReserveItem(item.id)}
+                                                        >
+                                                            <BsFillCartFill size="16" />
+                                                        </button>
+                                                    )}
+                                                    {item.reserved && loggedInUser != null && loggedInUser.id !== user.id && (
+                                                        <>
+                                                            {item.Reservations[0].User.id === loggedInUser.id && (
+                                                                <button
+                                                                    className="ml-2 px-2 py-1 bg-red-500 text-white rounded-md shadow-md hover:bg-red-600 focus:outline-none"
+                                                                    onClick={() => handleCancelReserveItem(item.id)}
+                                                                >
+                                                                    <BsCartX size="16" />
+                                                                </button>
+                                                            )}
+                                                        </>
+                                                    )}
+                                                    {loggedInUser.id === item.userId && (
+                                                        <>
+                                                            <button onClick={() => handleEditItemClick(item)} className="ml-2 px-2 py-1 bg-red-500 text-white rounded-md shadow-md hover:bg-red-600 focus:outline-none">
+                                                                <BsPencil size="16" />
+                                                            </button>
+                                                            <button onClick={() => handleDeleteItemClick(item)} className="ml-2 px-2 py-1 bg-red-500 text-white rounded-md shadow-md hover:bg-red-600 focus:outline-none">
+                                                                <BsTrashFill size="16" />
+                                                            </button>
+                                                        </>
+                                                    )}
+                                                </div>
+                                            </li>
+                                        ))}
+                                </ul>
+                                {loggedInUser != null && loggedInUser.id === user.id && (
+                                    <button className="px-2 mt-1 py-2 bg-emerald-400 text-white rounded-md shadow-md hover:bg-emerald-600 min-w-full focus:outline-none" onClick={handleAddItem}> + </button>
+                                )}
+                            </div>
+                        ))}
                     </Masonry>
                 </div>
             )}
